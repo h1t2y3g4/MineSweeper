1130f07962d30e5bd35a4f0534af7050c34eefcd
h1t2y3g4 <35458452+h1t2y3g4@users.noreply.github.com>
2018-04-05 21:51:03 +0800

    二维列表储存地雷
    这样在之后生成地雷的时候方便一点

--- functions.py
+++ functions.py
@@ -20,21 +20,23 @@ def check_event(blocks, setting):
 
 # 当鼠标点击了没有翻开的地雷的时候，做出反应
 def response_mouse_click(mouse_x, mouse_y, blocks, setting):
-    for block in blocks:
-        if not block.clicked_flag:
-            if block.rect.top <= mouse_y <= block.rect.bottom and block.rect.left <= mouse_x <= block.rect.right:
-                block.change_clicked_flag()
-                block.image.fill(setting.clicked_mine_block_color)
+    for block_y in blocks:
+        for block in block_y:
+            if not block.clicked_flag:
+                if block.rect.top <= mouse_y <= block.rect.bottom and block.rect.left <= mouse_x <= block.rect.right:
+                    block.change_clicked_flag()
+                    block.image.fill(setting.clicked_mine_block_color)
 
 
 # 当鼠标移动到没有翻开的方块上时作出反应
 def response_mouse_position(mouse_x, mouse_y, blocks, setting):
-    for block in blocks:
-        if not block.clicked_flag:
-            if block.rect.top <= mouse_y <= block.rect.bottom and block.rect.left <= mouse_x <= block.rect.right:
-                block.image.fill(setting.mine_block_color_mousemotion)
-            else:
-                block.image.fill(setting.mine_block_color)
+    for block_y in blocks:
+        for block in block_y:
+            if not block.clicked_flag:
+                if block.rect.top <= mouse_y <= block.rect.bottom and block.rect.left <= mouse_x <= block.rect.right:
+                    block.image.fill(setting.mine_block_color_mousemotion)
+                else:
+                    block.image.fill(setting.mine_block_color)
 
 
 # 创建地雷列表
@@ -47,17 +49,20 @@ def create_blocks(field, screen_main, setting):
         为什么不只创建一次实例，然后通过while循环累加两个坐标值，每循环一次就add一次到编组中呢？这关系到python动态语言的性质，
     放到这个例子中来大概就是说，不能通过实例对象调用+=这种自身的运算来给类属性赋值，但可以通过类对象来进行这种运算赋值。哎呀
     绕晕了，查到的资料也没怎么看懂。http://python.jobbole.com/85100/
+        blocks[y][x]。这样的坐标表示就是先处理行，再处理列，这样更符合习惯一点。
     """
     for block_number_y in range(block.number_y):
+        blocks.append([])
         for block_number_x in range(block.number_x):
             block = MineBlock(field, screen_main, setting)
             block.rect.x = block.field.rect.left + 1 + block_number_x * block.len
             block.rect.y = block.field.rect.top + 1 + block_number_y * block.high
-            blocks.append(block)
+            blocks[block_number_y].append(block)
     return blocks
 
 
 # 将地雷列表会知道主屏幕上面去
 def update_blocks(blocks):
-    for block in blocks:
-        block.built_me()
+    for block_y in blocks:
+        for block in block_y:
+            block.built_me()
