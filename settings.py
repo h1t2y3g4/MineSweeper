74805b4df7cca834ded8bee03dccd16ff6fa3759
h1t2y3g4 <35458452+h1t2y3g4@users.noreply.github.com>
2018-04-05 17:59:39 +0800

    列表储存地雷
    鼠标反应已经建好

--- settings.py
+++ settings.py
@@ -13,11 +13,16 @@ class Setting:
         # 设置地雷小方块的参数。
         self.mine_window_size = (24, 24)
         self.mine_block_color = (43, 87, 154)
-        self.mine_block_color_mousemotion = (101, 169, 241)
-        self.mousemotion_flage = False
+        self.mine_block_color_mousemotion = (101, 169, 241)  # 鼠标放在上面时的地雷颜色
+        self.clicked_mine_block_color = (193, 204, 227)  # 翻开后的地雷方块颜色
+
+        # 设置记录窗口参数
+        self.record_window_size = (200, 50)
+        self.record_window_color = (0, 0, 0)
 
         # 设置帧数
         self.fps = 60
 
-
+        # 设置地雷数
+        self.mine_number = 10
 
